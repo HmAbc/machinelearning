30766a049e05545331a75f58a3a723e920fc7c90
HmAbc <houminghua3521@gmail.com>
2020-12-06 21:28:13 +0800

    多元分类

--- ex3-neural network/neural network.py
+++ ex3-neural network/neural network.py
@@ -57,6 +57,7 @@ def gradient(theta, X, y, learnrating, m):
 
     grad = ((X.T * error) / m).T + (learnrating / m) * theta
     grad[0, 0] = np.sum(np.multiply(error, X[:, 0])) / m
+
     return np.array(grad).ravel()
 
 # 一对多分类
@@ -77,13 +78,14 @@ def one_vs_all(X, y, num_label, learningrate, m):
 
     return all_theta
 
-all_theta = one_vs_all(data['X'], data['y'], 10, 1, m)
+all_theta = one_vs_all(data['X'], data['y'], 10, 0.1, m)
 
 
 def predict_all(X, all_theta):
     rows = X.shape[0]
     params = X.shape[1]
     num_labels = all_theta.shape[0]
+    print(all_theta.shape)
 
     # same as before, insert ones to match the shape
     X = np.insert(X, 0, values=np.ones(rows), axis=1)
@@ -94,9 +96,10 @@ def predict_all(X, all_theta):
 
     # compute the class probability for each class on each training instance
     h = sigmoid(X * all_theta.T)
-
+    print(h.shape)
     # create array of the index with the maximum probability
     h_argmax = np.argmax(h, axis=1)
+
 
     # because our array was zero-indexed we need to add one for the true label prediction
     h_argmax = h_argmax + 1
